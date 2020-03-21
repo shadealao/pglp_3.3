3b477c2152668aa6dc76a6dcfab3b77a83d31c3a
shadealao <alao.a.s@gmail.com>
2020-03-21 18:04:47 +0100

    projet init mvn

---
+++ src/main/java/fr/uvsq21506437/lsp/App.java
@@ -0,0 +1,13 @@
+package fr.uvsq21506437.lsp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
