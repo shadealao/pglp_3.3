c409c1c33327e609478a9fcc1c16fe97a4f6f74d
shadealao <alao.a.s@gmail.com>
2020-03-30 14:25:16 +0200

    ajout testgestposition()

--- src/test/java/fr/uvsq21506437/lsp/AppTest.java
+++ src/test/java/fr/uvsq21506437/lsp/AppTest.java
@@ -1,38 +1,21 @@
 package fr.uvsq21506437.lsp;
 
-import junit.framework.Test;
+import java.util.ArrayList;
+
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class AppTest extends TestCase{
+    
+	public void testGetPosition() {
+		ArrayList<Robot> Robots = new ArrayList<Robot>();
+		Robots.add(new RobotType1(20));
+		Robots.add(new RobotType2(20));
+		for(int i = 0; i <Robots.size(); i++) Robots.get(i).avance();
+		assertEquals(25, Robots.get(0).position);
+		assertEquals(30, Robots.get(1).position);
+		
+	}
 }
